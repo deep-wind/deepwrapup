c396d1a77b5aa33b8456bb2ac616a928f3f05114
deep-wind <78983104+deep-wind@users.noreply.github.com>
2022-07-17 03:16:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,11 @@ for i in listdir:
     all_files = os.listdir(i)
     
     for f in all_files:
-        os.remove(f)
+	try:
+	    os.remove(f)
+	except os.error:
+	    pass
+
 	
 def read_pdf(file):
 	pdfReader = PdfFileReader(file)
