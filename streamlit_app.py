c248a518411e1d9364ce6ec10ffca0a14a222e0d
deep-wind <78983104+deep-wind@users.noreply.github.com>
2022-07-17 03:31:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import jellyfish
 import textwrap
+from gtts import gTTS
                     
 import moviepy.video.io.ImageSequenceClip                     
 nltk.download('punkt')
